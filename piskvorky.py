8185badbf7f9913888e98f050b847781aad69362
Xendrra <47497023+Xendrra@users.noreply.github.com>
2019-11-11 23:42:53 +0100

    Add files via upload

--- piskvorky.py
+++ piskvorky.py
@@ -1,32 +1,50 @@
 from random import randrange
-znak=input("x nebo o?")
-pozice=0
-if znak=="x":
-    pc="o"
-elif znak=="o":
-    pc="x"
-else: pc="x"
-print("Tvůj znak je ",znak,". Počítač má znak ",pc)
-pole=20*"-"
+znak = input("x nebo o?")
+pozice = 0
+if znak == "x":
+    pc = "o"
+elif znak == "o":
+    pc = "x"
+else:
+    pc = "x"
+print("Tvůj znak je ", znak, ". Počítač má znak ", pc)
+pole = 20*"-"
 def tah():
     global b
-    pozice=int(input("zadej pozicu:  "))
-    if pole[pozice]==znak or pole[pozice]==pc:
+    pozice = int(input("zadej pozicu:  "))
+    if pole[pozice] == znak or pole[pozice] == pc:
         print("Tady uz je obsazeno")
-        b=pole
+        b = pole
         tah()
     else:
-        b=pole[:pozice]+znak+pole[pozice+1:]
+        b = pole[:pozice] + znak + pole[pozice + 1:]
 def ptah():
     global c
-    c=randrange(0,len(pole)+1)
-        if pole[c]==znak or pole[c]==pc:
-            ptah()
-        else:
-            b=pole[:c]+pc+pole[c+1:]
+    global d
+    c = randrange(0, len(pole))
+    if pole[c] == znak or pole[c] == pc:
+        ptah()
+    else:
+        d = pole[:c] + pc + pole[c + 1:]
 
+def vyhodnot():
+    global q
+    if (3 * znak in pole) == True:
+        print("Gratuluji, vyhravas!")
+        q = False
+    elif (3 * pc in pole) == True:
+        print("maybe next time bro")
+        q = False
+    else:
+        q = True
 while True:
     tah()
-    print("Stare pole je ",pole)
-    pole=b
-    print("Snove pole je ",pole)
+    print("Stare pole je ", pole)
+    pole = b
+    vyhodnot()
+    print("Snove pole je ", pole)
+    ptah()
+    vyhodnot()
+    print("Stare pole je ", d)
+    if q == False:
+        break
